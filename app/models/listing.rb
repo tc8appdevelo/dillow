940aa9b43938c99b0f708b2a4ea95ef88c6d3e4b
tc8appdevelo <tc8appdevelo@gmail.com>
2022-01-14 09:15:44 -0600

    added seeds

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -15,10 +15,10 @@ class Listing < ApplicationRecord
 
     has_one_attached :photo
 
-    def ensure_photo
-        unless self.photo.attached?
-            errors[:photo] << "must be attached"
-        end
-    end
+    # def ensure_photo
+    #     unless self.photo.attached?
+    #         errors[:photo] << "must be attached"
+    #     end
+    # end
     
 end
