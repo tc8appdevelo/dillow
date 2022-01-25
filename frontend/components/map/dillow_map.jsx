964b1d2d755498717e04e53218f80ef003649da6
tc8appdevelo <tc8appdevelo@gmail.com>
2022-01-25 04:42:44 -0600

    used Geocoding to create map marker from address

--- frontend/components/map/dillow_map.jsx
+++ frontend/components/map/dillow_map.jsx
@@ -4,42 +4,46 @@ import React from 'react';
 class DillowMap extends React.Component {
   constructor(props) {
     super(props)
-    
   }
-  
-  componentDidMount() {
-    // set the map to show SF
-    
+
+  createMap() {
     const mapOptions = {
-      center: { lat: 32.7767, lng: -96.7970 }, // this is SF
+      center: { lat: 32.7767, lng: -96.7970 },
       zoom: 13
     };
 
-    
-    this.map = new google.maps.Map(this.mapNode, mapOptions);
+    const map = new google.maps.Map(this.mapNode, mapOptions);
+    const testPos = { lat: 32.7777, lng: -96.7980 }
+    const marker = new google.maps.Marker({
+      position: testPos,
+      map: map
+    });
 
-    // let m = { position: new google.maps.LatLng(32.7867, -96.7970)}
-    // let mk1 = new google.maps.Marker(m);
+    const geocoder = new google.maps.Geocoder();
+    let address = "7818 Caruth Ct, Dallas, TX 75225"
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status == 'OK') {
+        var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+        });
+      } else {
+        console.log('Geocode was not successful for the following reason: ' + status);
+      }
+    })
+  }
 
-    this.map = new google.maps.Map(this.mapNode, mapOptions);
-    // let m2 = { position: new google.maps.LatLng(32.7667, -96.7870) }
-    // let mk2 = new google.maps.Marker(m2);
-    // mk1.setMap(this.map);
-
-    // mk2.setMap(this.map);
-
-
+  componentDidMount() {
+    this.createMap();
   }
 
   render() {
 
-    
-
     return( 
       <div id="map" ref={ map => this.mapNode = map}></div>
     )
-  }
 
+  }
 }
 
 
